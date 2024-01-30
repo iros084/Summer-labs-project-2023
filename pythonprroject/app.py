2a902748faa1527b3c0fac33ef59730e9c0aed92
Iain Ross <iros084@aucklanduni.ac.nz>
2024-01-30 13:30:50 +1300

    added changes

--- pythonprroject/app.py
+++ pythonprroject/app.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def home():
     return render_template('home.html')
+
+@app.route('/signup', methods = ['GET', 'POST'])
+def about():
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
